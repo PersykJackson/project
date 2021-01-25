0457507a97cd4fa6fd9e28f1ecd39471c05b9c79
Zagidullin Vladislav <persyk32111@gmail.com>
2021-01-25 01:34:29 -0800

    fixed flying footer

--- public/layout.php
+++ public/layout.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossorigin="anonymous">
-    <link rel='stylesheet' href='style/index.css'>
+    <link rel='stylesheet' href='style/register.css'>
     <link rel="stylesheet" href="style/layout.css">
 </head>
 <body>
