2aabfc20674b7267ab0b397ca663fa69adad8a84
Zagidullin Vladislav <persyk32111@gmail.com>
2021-01-25 01:56:23 -0800

    products is isolated

---
+++ public/views/products.php
@@ -0,0 +1,26 @@
+<main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+
+                <div class="products">
+                    <h3>Товары</h3><hr/>
+                    <div class="row">
+                        <?php foreach($this->content['products'] as $product) {
+                            echo "
+                            <div class='col-4 col-md-2 product'>
+                            <img class='rounded img-fluid product-img' src='".$product['img']."'>
+                            <p>{$product['name']}</p>
+                            <strong>{$product['cost']} грн</strong><br/>
+                            <button>В корзину</button>
+                        </div>
+                            ";
+                        }?>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</main>
