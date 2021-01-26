c9b4e9916d0a33cbc3a3c2d97e8d040649572ce4
Zagidullin Vladislav <persyk32111@gmail.com>
2021-01-26 03:49:22 -0800

    create product searcher

--- products.php
+++ products.php
@@ -1,61 +1,73 @@
 <?php
 return [
     [
+        'id' => 1,
         'name' => 'Плюшевый мишка',
         'img' => '../images/products/teddy-bear.jpg',
         'cost' => '350'
     ],
     [
+        'id' => 2,
         'name' => 'Игровое кресло',
         'img' => '../images/products/dxracer.jpg',
         'cost' => '2700'
     ],
     [
+        'id' => 3,
         'name' => 'Напиток',
         'img' => '../images/products/coca-cola.jpg',
         'cost' => '16'
     ],
     [
+        'id' => 4,
         'name' => 'Телефон Asus',
         'img' => '../images/products/phone.jpg',
         'cost' => '6700'
     ],
     [
+        'id' => 5,
         'name' => 'Монитор Acer',
         'img' => '../images/products/acer.jpg',
         'cost' => '9400'
     ],
     [
+        'id' => 6,
         'name' => 'Свитер',
         'img' => '../images/products/sweter.jpg',
         'cost' => '760'
     ],
     [
+        'id' => 7,
         'name' => 'Плюшевый мишка',
         'img' => '../images/products/teddy-bear.jpg',
         'cost' => '350'
     ],
     [
+        'id' => 8,
         'name' => 'Игровое кресло',
         'img' => '../images/products/dxracer.jpg',
         'cost' => '2700'
     ],
     [
+        'id' => 9,
         'name' => 'Напиток',
         'img' => '../images/products/coca-cola.jpg',
         'cost' => '16'
     ],
     [
+        'id' => 10,
         'name' => 'Телефон Asus',
         'img' => '../images/products/phone.jpg',
         'cost' => '6700'
     ],
     [
+        'id' => 11,
         'name' => 'Монитор Acer',
         'img' => '../images/products/acer.jpg',
         'cost' => '9400'
     ],
     [
+        'id' => 12,
         'name' => 'Свитер',
         'img' => '../images/products/sweter.jpg',
         'cost' => '760'
