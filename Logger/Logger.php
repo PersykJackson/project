7c6805444560062efd31298d871e2c78526eb4f0
Zagidullin Vladislav <persyk32111@gmail.com>
2021-01-26 10:44:26 -0800

    Created logger

---
+++ Logger/Logger.php
@@ -0,0 +1,58 @@
+<?php
+
+
+class Logger implements LoggerInterface
+{
+    private string $fileName;
+    public function __construct($fileName = 'logs.log')
+    {
+        $this->fileName = $fileName;
+    }
+
+    public function emergency($message, array $context = [])
+    {
+        $this->log(LogLevel::EMERGENCY, $message, $context);
+    }
+
+    public function alert($message, array $context = [])
+    {
+        $this->log(LogLevel::ALERT, $message, $context);
+    }
+
+    public function critical($message, array $context = [])
+    {
+        $this->log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    public function error($message, array $context = [])
+    {
+        $this->log(LogLevel::ERROR, $message, $context);
+    }
+
+    public function warning($message, array $context = [])
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+    public function notice($message, array $context = [])
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    public function info($message, array $context = [])
+    {
+        $this->log(LogLevel::INFO, $message, $context);
+    }
+
+    public function debug($message, array $context = [])
+    {
+        $this->log(LogLevel::DEBUG, $message, $context);
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        $now = date('Y-m-d H:i:s');
+        $file = "\n".$now.' '.$level.' - '.$message.' - '.implode($context);
+        file_put_contents(__DIR__.'/logs/'.$this->fileName, $file, FILE_APPEND);
+    }
+}
