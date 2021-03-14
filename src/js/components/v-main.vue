64d8be70756b5b7cce9df56b19eb68d08d4df5fa
PersykJackson <persyk32111@gmail.com>
2021-03-14 05:15:49 -0700

    add carousel on main page

--- src/js/components/v-main.vue
+++ src/js/components/v-main.vue
@@ -2,7 +2,24 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-12 new-year-img">
-        <img class="img-fluid rounded mx-auto d-block" src="/images/new-year-image.jpg" alt="oops"/>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/images/happy-birthday.jpg" class="d-block w-100" alt="img">
+          </div>
+          <div class="carousel-item">
+            <img src="/images/women-day.jpg" class="d-block w-100" alt="img">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
       </div>
       <div class="col-12 popular">
         <div class="products">
